49998477df69064936609c4b6d9a96b9f6048e32
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-04-02 18:55:56 +0545

    Python-learnings

---
+++ Chapter-1/Chapter-1-PS/Problem2.py
@@ -0,0 +1,1 @@
+#USE REPL to print table of 5 
--- Chapter-1/module.py
+++ Chapter-1/module.py
@@ -1,4 +1,13 @@
 import pyjokes
 joke = pyjokes.get_joke()
+# THIS PRINTS A RANDOM JOKE
 print("Pinting joke......loading....")
 print(joke)
+
+
+"""
+Its a multilined comment
+"""
+# This is a sinle line comment
+
+ 
