d49c8fcc11bb80d2c0a4e3e11593fa07158803c3
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-04-04 09:33:03 +0545

    learnings

---
+++ Chapter-2/Chapter-2-PS/problem3.py
@@ -0,0 +1,3 @@
+a= input("Enter a number1:")
+t= type(a)
+print(t)
---
+++ Chapter-2/Chapter-2-PS/problem5.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a 1st number :"))
+b=int(input("Enter a 2nd number :"))
+average= (a+b)/2
+print("Average of given two number is;",average)
